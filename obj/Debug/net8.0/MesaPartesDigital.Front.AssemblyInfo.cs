b838cbe59ab4ffc13e78b1fc369c8b52e9a8de5d
Carlos Mora <carlosamora98@gmail.com>
2026-08-12 14:39:21 -0500

    feature_12_v.3

--- obj/Debug/net8.0/MesaPartesDigital.Front.AssemblyInfo.cs
+++ obj/Debug/net8.0/MesaPartesDigital.Front.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MesaPartesDigital.Front")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a058db147ef88c61c340df3be1e3fe2b18ed9610")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32479a378530ae853fd5bcf07cc9a243fc1124f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MesaPartesDigital.Front")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MesaPartesDigital.Front")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
